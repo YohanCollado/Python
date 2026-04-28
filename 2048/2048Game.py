fb528276f5d1adac4879ffa1d86ecb449cbb495e
Yohan Pablo Collado Suri <yohancollado@yahoo.com>
2026-04-27 21:19:08 -0400

    Keep it up

--- 2048/2048Game.py
+++ 2048/2048Game.py
@@ -6,15 +6,15 @@ import random
 #main class
 class Play_2048(Tk):
 
-    game_board = []
-    new_random_tiles = [2,2,2,2,2,2,4]
-    score = 0
-    high_score = 0
-    game_score = 0
-    highest_score = 0
+      game_board = []
+      new_random_tiles = [2,2,2,2,2,2,4]
+      score = 0
+      high_score = 0
+      game_score = 0
+      highest_score = 0
 
-    #user window
-    def __init__(self, *args, **kwargs):
+      #user window
+      def __init__(self, *args, **kwargs):
         Tk.__init__(self, *args, **kwargs)
         # UI
         self.game_score = StringVar(self)
@@ -25,8 +25,8 @@ class Play_2048(Tk):
         #new game, score and highest score
         self.button_frame = Frame(self)
         self.button_frame.grid(row=2, column=0, columnspan=4)
-        #Button(self.button_frame, text="New Game", font=("times new roman", 15), 
-               #command=self.new_game).grid(row=0, column=0)
+        Button(self.button_frame, text="New Game", font=("times new roman", 15), 
+               command=self.new_game).grid(row=0, column=0)
         self.button_frame.pack(side="top")
 
         Label(self.button_frame, text="Score:", 
@@ -43,7 +43,104 @@ class Play_2048(Tk):
         self.canvas.pack(side="top", 
                          fill="both", expand="false")
         #create new game
-        #self.new_game()
+        self.new_game()
+
+
+
+      # adding a new tile for the game
+      def new_tiles(self):
+            index = random.randint(0, 6)
+            x = -1
+            y = -1
+
+            #check while the game is not over
+            while self.full() == False:
+                  x = random.randint(0, 3)
+                  y = random.randint(0, 3)
+
+                  if (self.game_board[x][y] == 0):
+                      self.game_board[x][y] == self.new_random_tiles[index]
+                      x1 = y * 105
+                      y1 = x * 105
+                      x2 = x1 + 105 - 5
+                      y2 = y1 + 105 - 5
+                      num = self.game_board[x][y]
+                      if num == 2:
+                        self.square[x, y] = self.canvas.create_rectangle(x1, y1, x2, y2, full="#e0f2f8", tags="rect", outline="", width=0)
+                        self.canvas.create_text((x1 + x2) / 2, (y1 + y2) / 2, font=("Ariel", 36), fill="#f78a8a", text="2")
+                      elif num == 4:
+                        self.square[x, y] = self.canvas.create_rectangle(x1, y1, x2, y2, fill="b8dbe5", tags="react", outline="", width=0)
+                        self.canvas.create_text((x1 + x2) / 2, (y1 + y2) / 2, font=("Ariel", 36), fill="#f78a8a", text="4")
+                      break;
+      
+      # this section will check if all spaces in the game are full
+      def show_board(self):
+            cellwidth = 105
+            cellheight = 105
+            self.square = {}
+
+            for column in range(4):
+                  for row in range(4):
+                        x1 = column * cellwidth
+                        y1 = row * cellwidth
+                        x2 = x1 + cellwidth - 5
+                        y2 = x2 + cellwidth - 5
+                        num = self.game_board[row]
+                        if num == 0:
+                              self.show_number0(row, column, x1, y1, x2, y2)
+                        else:
+                              self.show_number(row, column, x1, y1, x2, y2, num)      
+                  
+      # show board when it is empty
+      def show_number0(self, row, column, a, b, c, d):
+            self.square[row, column] = self.canvas.create_rectangle(a, b, c, d, fill="#f5f5f5", tags="rect", outline="")
+      
+      #show board numbers
+      def show_number(self, row, column, a, b, c, d, num):
+            bg_color = {'2': '#eee4da', '4': '#ede0c8', '8': '#edc850', '16': '#edc53f', '32': '#f67c5f', '64': '#f65e3b', 
+                        '128': '#edcf72', '256': '#edcc61', '512': '#f2b179', '1024': '#f59563', '2048': '#edc22e',}
+            color = {'2': '#776e65', '4': '#f9f6f2', '8': '#f9f6f2', '16': '#f9f6f2', '32': '#f9f6f2', '64': '#f9f6f2', 
+                        '128': '#f9f6f2', '256': '#f9f6f2', '512': '#776e65', '1024': '#f9f6f2', '2048': '#f9f6f2', }
+            self.square[row, column] = self.canvas.create_rectangle(a, b, c, d, fill=bg_color[str(num)], tags="rect", outline="")
+            self.canvas.create_text((a + c) / 2, (b + d) / 2, font=("Arial", 36), fill=color[str(num)], text=str(num))
+
+      
+
+
+
+
+
+
+
+
+      # func to create a new game
+      def new_game(self):
+            self.score = 0
+            self.game_score.set("0")
+            self.game_board = []
+            self.game_board.append([0, 0, 0, 0])
+            self.game_board.append([0, 0, 0, 0])
+            self.game_board.append([0, 0, 0, 0])
+            self.game_board.append([0, 0, 0, 0])
+            while True:
+                  x = random.randint(0, 3)
+                  y = random.randint(0, 3)
+                  if (self.game_board[x][y] == 0):
+                        self.game_board[x][y] == 2
+                        break
+            index = random.randint(0, 3)
+            while self.full() == False:
+                  x = random.randint(0, 3)
+                  y = random.randint(0, 3)
+                  if (self.game_board[x][y] == 0):
+                        self.game_board[x][y] = self.new_random_tiles[index]
+                        break
+                  self.show_board()
+
+
+
+
+
 
 
 
