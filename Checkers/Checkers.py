d40c891e065db99344978c9fdded0c413824b61e
YohanCollado <yohancollado@yahoo.com>
2026-04-29 21:56:12 -0400

    Made a view

--- Checkers/Checkers.py
+++ Checkers/Checkers.py
@@ -1,17 +1,88 @@
-from tkinter import *
+from tkinter import * # imports everything from libary 
+from PIL import Image, ImageTk
 
-class Checkers(Tk):
-    def __init__(self):
-        super().__init__()
+class Checkers(Tk): # creating a class called checkers that inherets from TK
+    def __init__(self): # runs when create Checkers()
+        super().__init__() # calls parent tk constructor
         
         self.title("Checkers")
-        self.minsize(500, 500)
+        self.minsize(640, 640) # stay at this size usually
 
-        top = Frame(self)
-        top.pack(side="top")
+        top = Frame(self) # creates containers
+        top.pack(side="top") # sets that container to top
 
-        Button(top, text="Reset", font=("Aril", 15)).grid(row=0, column=1)
+        Button(top, 
+               text="Reset", 
+               font=("Ariel", 15)).grid(row=0, column=1) # button to reset game, need to add command after
+        
+        self.board() # this is what draws the board, without this the window is just blank
+        self.image_piece()
+        self.set_piece()
 
-if __name__ == "__main__":
-    app = Checkers()
-    app.mainloop()
+    def board(self): # method to make board
+        rows = 8 
+        columns = 8
+        square_size = 80 # each square on the board is of 80 pixels
+
+        white = "#FFFFFF" # white color
+        black = "#4B4B4B" # black color for board
+
+        self.canvas = Canvas( # creates a canvas (area to create), attached to self which is the main window
+            self,
+            width = columns * square_size, # the width is 8 * 860 which is 640
+            height = rows * square_size # same for height
+        )
+        self.canvas.pack() # displays the canvas in the window
+
+        for row in range(rows): # loops through the rows
+            for column in range(columns): # loops through columns
+                x1 = column * square_size
+                y1 = row * square_size
+                x2 = x1 + square_size
+                y2 = y1 + square_size
+
+                if (row + column) % 2 == 0: # displays the color
+                    color = white
+                else:
+                    color = black
+                
+                self.canvas.create_rectangle( # draws a rectangle in the canvas and fills colors
+                    x1, y1, x2, y2, 
+                    fill=color, 
+                    outline=color)
+                
+    def image_piece(self):    
+        
+        size = 80
+
+        red = Image.open("Checkers/Images/red.png")
+        black = Image.open("Checkers/Images/black.png")
+
+        red = red.resize((size, size))
+        black = black.resize((size, size))
+
+        self.red_piece = ImageTk.PhotoImage(red)
+        self.black_piece = ImageTk.PhotoImage(black)
+
+    def draw_piece(self, row, column, image):
+        square_size = 80
+        x = column * square_size + square_size // 2
+        y = row *square_size + square_size // 2
+
+        self.canvas.create_image(x, y, image=image)
+
+    def set_piece(self): 
+        for row in range(3):
+            for column in range(8):
+                if (row + column) % 2 == 1:
+                    self.draw_piece(row, column, self.red_piece)
+        for row in range(5, 8):
+            for column in range(8):
+                if (row + column) % 2 == 1:
+                    self.draw_piece(row, column, self.black_piece)
+
+
+
+if __name__ == "__main__": # makes code run
+    app = Checkers() # creates window, triggers, __init__
+    app.mainloop() #starts the GUI()
